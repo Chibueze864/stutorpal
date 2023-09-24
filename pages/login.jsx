d30a23c3d750aeb239991808037ceb5f16646ec2
Chibueze864 <66389146+Chibueze864@users.noreply.github.com>
2023-09-24 18:22:09 +0100

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/footer.jsx";
 import {useState, useEffect} from "react"
 import { useRouter } from "next/router"
 
-export default function login(){
+export default function Login(){
     const router = useRouter()
     const provider = new GoogleAuthProvider();
   const [email, setEmail] = useState("");
@@ -49,20 +49,20 @@ export default function login(){
                 {/* Form */}
        
     
-<div class="flex justify-center w-full  items-center">
-  <div class="bg-white shadow-md m-5 rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
-    <h2 class="text-3xl font-bold mb-6 text-center text-white">
-      <span class="text-black text-4xl font-semibold tracking-wide leading-none">
+<div className="flex justify-center w-full  items-center">
+  <div className="bg-white shadow-md m-5 rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
+    <h2 className="text-3xl font-bold mb-6 text-center text-white">
+      <span className="text-black text-4xl font-semibold tracking-wide leading-none">
         Login
       </span>
     </h2>
-    <div class="mt-2">
+    <div className="mt-2">
           <span className="text-red">{error}</span>
 
     </div>
-              <div class="mt-4">
-    <button onClick={signInWithGoogle} class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-101 duration-300 text-[#002D74]">
-        <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+              <div className="mt-4">
+    <button onClick={signInWithGoogle} className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-101 duration-300 text-[#002D74]">
+        <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
           <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
           <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
           <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
@@ -71,36 +71,36 @@ export default function login(){
         Login with Google
       </button>
     </div>
-      <div class="my-2">or</div>
+      <div className="my-2">or</div>
     
-      <div class="mb-6">
-        <label for="email" class="block text-gray-700 text-left text-sm font-bold mb-2">
-          <i class="fas fa-envelope mr-2"></i>Email
+      <div className="mb-6">
+        <label  className="block text-gray-700 text-left text-sm font-bold mb-2">
+          <i className="fas fa-envelope mr-2"></i>Email
         </label>
         <div>
-          <input id="email" onChange={(e)=>setEmail(e.target.value)} type="email" class="shadow-sm  border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"  pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" title="Invalid Email" placeholder="Enter your email" required/>
+          <input id="email" onChange={(e)=>setEmail(e.target.value)} type="email" className="shadow-sm  border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"  pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" title="Invalid Email" placeholder="Enter your email" required/>
         </div>
       </div>
-      <div class="mb-6">
-        <label for="password" class="block text-left text-gray-700 text-sm font-bold mb-2">
-          <i class="fas fa-lock mr-2"></i>Password
+      <div className="mb-6">
+        <label className="block text-left text-gray-700 text-sm font-bold mb-2">
+          <i className="fas fa-lock mr-2"></i>Password
         </label>
         <div>
-          <input id="password" onChange={(e)=>setPassword(e.target.value)}  type="password" class="shadow-sm  border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          <input id="password" onChange={(e)=>setPassword(e.target.value)}  type="password" className="shadow-sm  border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
   title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters" placeholder="Enter your password" required/>
         </div>
       </div>
-      <div class="flex items-center justify-center">
-        <button onClick={login} type="submit" class="bg-blue-500 text-white px-16 py-4 rounded-lg font-bold tracking-widest w-full hover:bg-blue-800">
+      <div className="flex items-center justify-center">
+        <button onClick={login} type="submit" className="bg-blue-500 text-white px-16 py-4 rounded-lg font-bold tracking-widest w-full hover:bg-blue-800">
           LogIn
         </button>
       </div>
-      <div class="text-center mt-4">
-        <a href="#" class="text-gray-600 hover:underline">Forgot password?</a>
+      <div className="text-center mt-4">
+        <a href="#" className="text-gray-600 hover:underline">Forgot password?</a>
       </div>
  
     
-    <p class="text-center text-gray-600 mt-6">Don't have an account? <a href="/register" class="text-blue-500 hover:underline">Sign up</a></p>
+    <p className="text-center text-gray-600 mt-6">You do not have an account? <a href="/register" className="text-blue-500 hover:underline">Sign up</a></p>
   </div>
 </div>
 
