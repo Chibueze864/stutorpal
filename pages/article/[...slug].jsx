ec23f9567ac5133fd427b26e3e2a4e1ba85353e9
Chibueze864 <66389146+Chibueze864@users.noreply.github.com>
2023-09-24 18:28:05 +0100

    Update [...slug].jsx

--- pages/article/[...slug].jsx
+++ pages/article/[...slug].jsx
@@ -67,41 +67,41 @@ querySnapshot.forEach((doc) => {
 <div>
     <div className="lg:px-20 py-8" style={{ backgroundImage: "url(https://stutorpal.github.io/waitlist/img/banner-4.png)", backgroundSize: "cover" ,zIndex:-1, position:"relative"}} >
       { blogPost && 
-  <div   class="max-w-screen-xl mx-auto p-8 sm:p-10 md:p-16 relative">
+  <div   className="max-w-screen-xl mx-auto p-8 sm:p-10 md:p-16 relative">
   
-  <div class="mb-5 max-w-2xl mx-auto">
-    <h1 href="#" class="text-gray-900 pb-4 font-bold text-3xl mb-2">{blogPost.header}</h1>
+  <div className="mb-5 max-w-2xl mx-auto">
+    <h1 href="#" className="text-gray-900 pb-4 font-bold text-3xl mb-2">{blogPost.header}</h1>
 
-    <div class="pb-4 text-gray-700 text-xs flex my-6 flex justify-between">
-      <div class="flex items-center">
-      <a href="#"><img class="w-12 h-12 rounded-full mr-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROXyfGAFLE7uuIrF6Qmi7QKmL6tBgx9Hs7SzWUg50AFw&s" alt="Avatar of Jonathan Reinink pb-2"/></a>
-      <div class="text-sm">
-        <a href="#" class="text-gray-900 font-medium leading-none hover:text-indigo-600 transition duration-500 ease-in-out pb-2">{blogPost.author}</a>
-        <p class="text-gray-600 text-xs pb-2">{blogPost.date}</p>
+    <div className="pb-4 text-gray-700 text-xs flex my-6 flex justify-between">
+      <div className="flex items-center">
+      <a href="#"><img className="w-12 h-12 rounded-full mr-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROXyfGAFLE7uuIrF6Qmi7QKmL6tBgx9Hs7SzWUg50AFw&s" alt="Avatar of Jonathan Reinink pb-2"/></a>
+      <div className="text-sm">
+        <a href="#" className="text-gray-900 font-medium leading-none hover:text-indigo-600 transition duration-500 ease-in-out pb-2">{blogPost.author}</a>
+        <p className="text-gray-600 text-xs pb-2">{blogPost.date}</p>
       </div>
     </div>
      </div>
   </div>
   
   
-  <div class="max-w-2xl mx-auto">
-    <div class="mt-3  rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
-   { blogPost.img &&    <div class="bg-cover  h-64 text-center overflow-hidden" style={dynamicStyles} title="Woman holding a mug">
+  <div className="max-w-2xl mx-auto">
+    <div className="mt-3  rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+   { blogPost.img &&    <div className="bg-cover  h-64 text-center overflow-hidden" style={dynamicStyles} title="Woman holding a mug">
             </div>}
-        <div class="">
-           <p class="text-base leading-8 my-10 break-words ">          {blogPost.content}
+        <div className="">
+           <p className="text-base leading-8 my-10 break-words ">          {blogPost.content}
 </p>
-              {/* <p class="text-base leading-8 my-10">
+              {/* <p className="text-base leading-8 my-10">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           
-          <h3 class="text-2xl font-bold my-5">#1. What is Lorem Ipsum?</h3>
+          <h3 className="text-2xl font-bold my-5">#1. What is Lorem Ipsum?</h3>
           
-          <p class="text-base leading-8 my-5">
+          <p className="text-base leading-8 my-5">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           
-          <blockquote class="text-md italic leading-8 my-5 p-8 text-indigo-600 font-semibold">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</blockquote>*/}
+          <blockquote className="text-md italic leading-8 my-5 p-8 text-indigo-600 font-semibold">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</blockquote>*/}
           
-          {/* <p class="text-base leading-8 my-5"> 
+          {/* <p className="text-base leading-8 my-5"> 
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
            */}
         
